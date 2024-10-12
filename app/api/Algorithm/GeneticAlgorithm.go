9eb60b275a4d84a3997aa97e6aa92126d77a207e
ValentinoTriadi <13522164@std.stei.itb.ac.id>
2024-10-12 23:08:00 +0700

    feat: genetic algorithm

---
+++ app/api/Algorithm/GeneticAlgorithm.go
@@ -0,0 +1,115 @@
+package Algorithm
+
+import (
+	"fmt"
+	"magic-cube/lib"
+	"time"
+)
+
+func GeneticAlgorithm(jumlah_populasi int, jumlah_generasi int) [125]int {
+	iterasi := 0
+	currentTime := time.Now()
+	populasi := lib.GeneratePopulation(jumlah_populasi)
+	// populasi_awal := populasi
+	bestState := [125]int{}
+	bestCost := 0
+
+	for iterasi < jumlah_generasi {
+		newGeneration := [][125]int{}
+
+		// Calculate the cost of the state
+		cost := make([]int, jumlah_populasi)
+		sumCost := 0
+		for i := 0; i < jumlah_populasi; i++ {
+			cost[i] = lib.ObjectiveFunction(populasi[i])
+			sumCost += cost[i]
+
+			if iterasi > 0 {
+				// Get best child of the best child of the best 👌
+				if cost[i] > bestCost {
+					bestCost = cost[i]
+					bestState = populasi[i]
+				}
+			}
+		}
+
+		// Selection
+		selectedPopulasi := make([][125]int, jumlah_populasi)
+		for i := 0; i < jumlah_populasi; i++ {
+			randPoint := lib.RandomInt(0, sumCost)
+			for j := 0; j < jumlah_populasi; j++ {
+				randPoint -= cost[j]
+				if randPoint <= 0 {
+					selectedPopulasi[i] = populasi[j]
+					break
+				}
+			}
+		}
+
+		// Crossover
+		for i := 0; i < jumlah_populasi; i += 2 {
+			// Randomly select 2 parents
+			parent1 := selectedPopulasi[i]
+			parent2 := selectedPopulasi[i+1]
+
+			crossoverPoint := 75
+
+			// Create 2 children
+			child1 := [125]int{}
+			child2 := [125]int{}
+
+			// Copy the first part of the parents to the children
+			for j := 0; j < crossoverPoint; j++ {
+				child1[j] = parent1[j]
+				child2[j] = parent2[j]
+			}
+
+			// Copy the second part of the parents to the children
+			currentIdx := 0
+			for j := crossoverPoint; j < 125; j++ {
+				for k := currentIdx; k < 125; k++ {
+					if lib.NotIn(parent2[k], child1) {
+						child1[j] = parent2[k]
+						currentIdx = k
+						break
+					}
+				}
+			}
+			currentIdx = 0
+			for j := crossoverPoint; j < 125; j++ {
+				for k := currentIdx; k < 125; k++ {
+					if lib.NotIn(parent1[k], child2) {
+						child2[j] = parent1[k]
+						currentIdx = k
+						break
+					}
+				}
+			}
+
+			// Mutate the children
+			// Swap two random elements in child1
+			idx1 := lib.RandomInt(0, 124)
+			idx2 := lib.RandomInt(0, 124)
+			child1[idx1], child1[idx2] = child1[idx2], child1[idx1]
+
+			// Swap two random elements in child2
+			idx1 = lib.RandomInt(0, 124)
+			idx2 = lib.RandomInt(0, 124)
+			child2[idx1], child2[idx2] = child2[idx2], child2[idx1]
+
+			newGeneration = append(newGeneration, child1)
+			newGeneration = append(newGeneration, child2)
+
+		}
+
+		populasi = newGeneration
+		iterasi++
+	}
+	fmt.Println("Time: ", time.Since(currentTime))
+	fmt.Println("Best Cost: ", bestCost)
+	// fmt.Println("Best State: ")
+	// lib.PrintState(bestState)
+
+	return bestState
+
+}
--- app/api/main.go
+++ app/api/main.go
@@ -16,7 +16,9 @@ func main() {
 
 	// Path
 	router.HandleFunc("/api/simulated_anneling", SimulatedAnneling).Methods("GET")
+	router.HandleFunc("/api/genetic_algorithm", GeneticAlgorithm).Methods("GET")
 	router.HandleFunc("/api/test_obj_func", TestObjFunc).Methods("GET")
+	router.HandleFunc("/api/test", Test).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
@@ -25,10 +27,19 @@ func SimulatedAnneling(w http.ResponseWriter, r *http.Request) {
 	Algorithm.SimulatedAnneling()
 }
 
+func GeneticAlgorithm(w http.ResponseWriter, r *http.Request) {
+	Algorithm.GeneticAlgorithm(1000, 1000)
+}
+
 func TestObjFunc(w http.ResponseWriter, r *http.Request) {
 	currentTime := time.Now()
 	for i := 0; i < 1000000; i++ {
 		lib.ObjectiveFunction([125]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125})
 	}
 	fmt.Println("Time: ", time.Since(currentTime))
+}
+
+func Test(w http.ResponseWriter, r *http.Request) {
+	lib.GeneratePopulation(10)
+	fmt.Println("Test")
 }
